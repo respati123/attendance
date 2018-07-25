d6da56f7e77130230ed799b0ebc70fecda77580c
Unknown <dhyatmika.suriyanto@gmail.com>
2018-07-25 16:46:51 +0700

    Recycler View Adapter WIP

---
+++ app/src/main/java/com/example/acer/attandance_free_feature/recyclerViewItem.java
@@ -0,0 +1,13 @@
+package com.example.acer.attandance_free_feature;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class recyclerViewItem extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.recycler_view_item);
+    }
+}
